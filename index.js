6b707e147505211a678d9181ee2d1cd374ca4e58
AvarageCodeEnjoyer <111468739+AvarageCodeEnjoyer@users.noreply.github.com>
2024-02-10 15:51:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ async function getData() {
   const results = await fetch(`https://graph.instagram.com/me/media?fields=media_url,caption,timestamp,media_type,permalink&access_token=${accessToken}`)
   const result = await results.json()
 
-  document.getElementById("id").innerHTML += result.data[postNumber].id
-  document.getElementById("caption").innerHTML += result.data[postNumber].caption
-  document.getElementById("mediaType").innerHTML += result.data[postNumber].media_type
-  document.getElementById("timestamp").innerHTML += result.data[postNumber].timestamp
-  document.getElementById("permalink").innerHTML += result.data[postNumber].permalink
+  document.getElementById("id").innerHTML = result.data[postNumber].id
+  document.getElementById("caption").innerHTML = result.data[postNumber].caption
+  document.getElementById("mediaType").innerHTML = result.data[postNumber].media_type
+  document.getElementById("timestamp").innerHTML = result.data[postNumber].timestamp
+  document.getElementById("permalink").innerHTML = result.data[postNumber].permalink
   document.getElementById('image').setAttribute("src", result.data[postNumber].media_url)
 }
